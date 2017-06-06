416e52ffed08d0ec7bdd867b00994a55367d430b
Adiv Ohayon <adivohayon@gmail.com>
2017-06-06 22:57:55 +0300

    desktop

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,17 @@
 console.log('\'Allo \'Allo!');
+
+$(document).ready(function() {
+	console.log('this is jquery');
+
+	var height = $('.section-1').outerHeight(); //gets height of header
+
+	$(window).scroll(function(){
+	    if($(window).scrollTop() > (height)){
+	       $('footer').addClass('fixed');
+	    }
+	    else{
+	       $('footer').removeClass('fixed');
+	    }
+	});
+ 
+});
