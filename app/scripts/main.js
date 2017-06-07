f5a860daaa59f0bb8d6f47878e563d6507b90fef
Adiv Ohayon <adivohayon@gmail.com>
2017-06-07 12:34:07 +0300

    final fixes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,19 +1,26 @@
 console.log('\'Allo \'Allo!');
 
+function isBreakpoint( alias ) {
+    return $('.device-' + alias).is(':visible');
+}
+
 $(document).ready(function() {
 	console.log('this is jquery');
-
+	$('footer').removeClass('fixed');
+	$('.section-3').removeClass('fixed');
 	var height = $('.section-1').outerHeight(); //gets height of header
-
-	$(window).scroll(function(){
-	    if($(window).scrollTop() > (height)){
-	       $('footer').addClass('fixed');
-	       $('.section-3').addClass('fixed');
-	    }
-	    else{
-	       $('footer').removeClass('fixed');
-	       $('.section-3').addClass('fixed');
-	    }
-	});
+	if( !isBreakpoint('xs') ) {
+		$(window).scroll(function(){
+		    if($(window).scrollTop() > (height)){
+		       $('footer').addClass('fixed');
+		       $('.section-3').addClass('fixed');
+		    }
+		    else{
+		       $('footer').removeClass('fixed');
+		       $('.section-3').removeClass('fixed');
+		    }
+		});
+	}
+	
  
 });
